1782d9fa926b40c5f5b990b31f41ded6594a0625
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-01-08 22:34:07 +0530

    arrays

--- arrays/runTimeInit.java
+++ arrays/runTimeInit.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 class runTimeInit
 {
-    public static void main(String[] args) {
-        int[] arr = new int[1000000];
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<arr.length;i++) //a.length or size
+        {
+            arr[i]=sc.nextInt();
         }
-        long end = System.currentTimeMillis();
-        System.out.println("Time taken to run the loop: " + (end - start) + "ms");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println("Element at index "+i+" is "+arr[i]);
+        }
     }
 }

1098189b5fb43a3e43b2ee0a3c32aaa707166d9b
Github <38401453+kiranraoboinapally@users.noreply.github.com>
2025-01-16 13:59:33 +0530

    Update arrays

---
+++ java/arrays/leftrotate.java
@@ -0,0 +1,11 @@
+class leftrotate
+{
+public static void main(String args[])
+  {
+    int a[]=new int[]{10,20,30,40,50,60};
+    for(int i=0;i<a.length-1;i++)
+    {
+    a[x]=a[x+1];// for rotation of 1time left
+    }
+  }
+}

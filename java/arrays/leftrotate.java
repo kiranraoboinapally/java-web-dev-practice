dad7c8ed42a62b8c4ca1ebaa534b6e6a0124a1bd
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-01-17 22:07:59 +0530

    new

--- java/arrays/leftrotate.java
+++ java/arrays/leftrotate.java
@@ -1,11 +1,21 @@
-class leftrotate
-{
-public static void main(String args[])
-  {
-    int a[]=new int[]{10,20,30,40,50,60};
-    for(int i=0;i<a.length-1;i++)
-    {
-    a[x]=a[x+1];// for rotation of 1time left
-    }
+class leftRotate {
+  public static void main(String args[]) {
+      int a[] = new int[]{10, 20, 30, 40, 50, 60};
+      
+      // Save the first element before the rotation
+      int firstElement = a[0];
+      
+      // Shift elements to the left by one position
+      for (int i = 0; i < a.length - 1; i++) {
+          a[i] = a[i + 1];
+      }
+      
+      // Place the first element at the last position
+      a[a.length - 1] = firstElement;
+      
+      // Print the rotated array
+      for (int i = 0; i < a.length; i++) {
+          System.out.print(a[i] + " ");
+      }
   }
 }

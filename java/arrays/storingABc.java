b31b68b36cd8abcf3efe1d980947c097c95ce7e9
Github <38401453+kiranraoboinapally@users.noreply.github.com>
2025-01-24 14:59:47 +0530

    Update storingABc.java
    updated

--- java/arrays/storingABc.java
+++ java/arrays/storingABc.java
@@ -2,8 +2,8 @@ class storingABc
 {
     public static void main(String[] args) 
     {
-        int a[]={1,2,3,4,5}
-        int b[]={6,7,8,9}
+        int a[]={1,2,3,4,5};
+        int b[]={6,7,8,9};
         int c[]=new int[a.length+b.length]; 
         for(int x=0;x<a.length;x++)
         {

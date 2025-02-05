da70a4d4d12d3a954adbe48336579833c4bb8e6e
Github <38401453+kiranraoboinapally@users.noreply.github.com>
2025-02-05 16:26:05 +0530

    Update twoSum.java

--- java/leetcode/easy/twoSum.java
+++ java/leetcode/easy/twoSum.java
@@ -13,3 +13,21 @@ class Solution {
         }return new int[] {}; 
     }
 }
+
+
+/*
+
+class Solution {
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 2, 7, 1, -2, 5, 0, 1, 9, 4};
+        int K = 5;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] + a[j] == K) {
+                    System.out.println(a[i] + "," + a[j]);
+                }
+            }
+        }
+    }
+}
+*/

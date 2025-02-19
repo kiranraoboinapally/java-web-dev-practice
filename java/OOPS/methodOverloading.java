e200b28d54b1d751e22bdbea211a5171ba404c81
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-02-19 21:52:06 +0530

    aOOps

--- java/OOPS/methodOverloading.java
+++ java/OOPS/methodOverloading.java
@@ -1,12 +1,30 @@
-class methodOverloading
+class MethodOverloading 
 {
-public static void main(String args[])
+public static void main(String args[]) 
 	{
-	Demo demo=new Demo();
-	
-	}
+        Demo demo = new Demo();
+        //Calling the overloaded methods
+        demo.display(10); //Calls the method with an integer argument
+        demo.display("Hello, World!"); //Calls the method with a string argument
+        demo.display(5,"Java"); //Calls the method with both integer and string arguments
+    }
 }
-class Demo
+class Demo 
 {
-
+//Method with one integer parameter
+void display(int x) 
+	{
+        System.out.println("Display integer: " + x);
+    	}
+// Method with one string parameter
+void display(String str) 
+	{
+        System.out.println("Display string: " + str);
+    	}
+//Method with both integer and string parameters
+void display(int x, String str) 
+	{
+        System.out.println("Display integer and string: " + x + ", " + str);
+    	}
 }
+
--- java/OOPS/problem3.java
+++ java/OOPS/problem3.java
@@ -1,1 +1,21 @@
 /*Can we overload the final method in java? */
+class P3 
+{
+//Final method
+final void display(int x) 
+	{
+        System.out.println("Displaying: " + x);
+    	}
+//Overloaded final method with a different parameter
+final void display(String s) 
+	{
+        System.out.println("Displaying: " + s);
+        }
+public static void main(String[] args) 
+	{
+        P3 obj = new P3();
+        obj.display(10);      //Calls display(int x)
+        obj.display("Hello"); //Calls display(String s)
+    	}
+}
+//using final we cant override it in subclass

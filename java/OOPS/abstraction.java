5b67f3612e110e7bd9b613b87cc59263abec91dd
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-02-20 15:36:16 +0530

    abstraction

--- java/OOPS/abstraction.java
+++ java/OOPS/abstraction.java
@@ -1,3 +1,14 @@
+
+//hiding internal implementationhighlighting functonality
+//Abstract method means has no implementation
+//for an abstract class cannot create an object
+//to do abstraction we need inheritance and override
+//abstraction can be achieved by abstract class and interface
+//if child is not providing implementation for the abstract method of parent then child class becomes abstract class
+//this continues until all the abstract methods are overriden or implemented.
+//abstract method of parent class need to be implemented by child class
+
+
 class Demo
 {
 public static void main(String args[])

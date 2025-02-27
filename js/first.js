66299b8eebc2793c77579523fe329dc82e7f72ea
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-02-27 23:40:42 +0530

    js

--- js/first.js
+++ js/first.js
@@ -1,3 +1,31 @@
 console.log('Hello all');
 console.log("I am learning JavaScript");
 console.log('This is my first JavaScript code')
+
+
+//variables are containers for data .
+
+fullName = "John Doe";
+age=25;
+console.log(fullName);
+console.log(age);
+
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+
+
+isFollow=false;
+console.log(isFollow);
+
+fullName= 25;//dynamically typed language
+console.log(fullName);
+
+
+//variable rules
+fullName= "John Smith";
+_fullName= "John Smith";
+$fullName= "John Smith";
+
+

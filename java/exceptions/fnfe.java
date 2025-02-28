e360c31eb7835b9165c2e1263e1c31914c5bbeae
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-03-01 00:05:10 +0530

    new

---
+++ java/exceptions/fnfe.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class fnfe
+{
+    public static void main(String[] args) 
+    {
+        String path="C:/Users/arunk/Desktop/chub/java/exceptions/age789.java";
+        try
+        {
+            Scanner sc=new Scanner(new File(path));
+            while(sc.hasNextLine())
+            {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch(FileNotFoundException e) //or Exception its parent class
+        {
+            System.out.println("File not found");
+        }
+    }
+}
+
+//if file found then it will print the content of the file i.e try block will execute
+//if file not found then it will print "File not found" i.e catch block will execute
+//if we don't write catch block then it will give compile time error i.e its checked exception
+

b1df930c6f53177cf012e364f15331ea98e98b45
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-03-03 22:22:11 +0530

    new

--- java/OOPS/interfaceHybrid.java
+++ java/OOPS/interfaceHybrid.java
@@ -1,23 +1,72 @@
 
-class Demo
-{
-public static void main(String args[])
-	{
-	}	
+interface One {
+    void methodOne();
+    default void defaultMethodOne() {
+        System.out.println("This is the default method in One.");
+    }
 }
-class GrandParent
-{
-
+interface Two {
+    void methodTwo();
+    default void defaultMethodTwo() {
+        System.out.println("This is the default method in Two.");
+    }
 }
-class Parent1 extends GrandParent
-{
 
+
+abstract class AbstractChildImpl implements One, Two {
+    @Override
+    public abstract void methodOne();
+
+    @Override
+    public abstract void methodTwo();
 }
-class Parent2 extends GrandParent
-{
 
+class ConcreteChild extends AbstractChildImpl {
+    @Override
+    public void methodOne() {
+        System.out.println("Implemented methodOne in ConcreteChild.");
+    }
+
+    @Override
+    public void methodTwo() {
+        System.out.println("Implemented methodTwo in ConcreteChild.");
+    }
 }
-abstract class AbstractChildImpl implements One,Two
-{
 
+class GrandParent {
+    void showGrandParent() {
+        System.out.println("GrandParent class method.");
+    }
+}
+
+
+class Parent1 extends GrandParent {
+    void showParent1() {
+        System.out.println("Parent1 class method.");
+    }
+}
+
+class Parent2 extends GrandParent {
+    void showParent2() {
+        System.out.println("Parent2 class method.");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        ConcreteChild child = new ConcreteChild();
+        child.methodOne();
+        child.methodTwo();
+
+        child.defaultMethodOne();
+        child.defaultMethodTwo();
+
+        Parent1 p1 = new Parent1();
+        p1.showGrandParent();
+        p1.showParent1();
+
+        Parent2 p2 = new Parent2();
+        p2.showGrandParent();
+        p2.showParent2();
+    }
 }
--- js/second.js
+++ js/second.js
@@ -11,3 +11,9 @@ console.log(k*b);
 console.log(k/b);
 console.log(k%b);
 console.log("a to power b ",k**b);
+
+let ab=5;
+let xy="5";
+console.log(ab==xy);//returns true
+console.log(ab===xy);
+console.log(ab!==xy);

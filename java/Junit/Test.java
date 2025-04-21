eaaa374f8b171d6f3acd4eafdfa403e8ecad0445
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-04-21 21:08:09 +0530

    Junit

---
+++ java/Junit/Test.java
@@ -0,0 +1,12 @@
+class Test 
+{
+public static boolean isOdd(int number) 
+	{
+        return number % 2 != 0;
+	}
+public static void main(String args[]) 
+	{
+        int testNumber = 5;  // You can change this to any integer
+        System.out.println(isOdd(testNumber));
+    	}
+}

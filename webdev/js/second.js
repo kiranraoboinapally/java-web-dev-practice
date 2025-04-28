f026e0cd410c9692f93e211a6c5c510577477bde
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-04-28 21:25:44 +0530

    shradha khapra

--- webdev/js/second.js
+++ webdev/js/second.js
@@ -12,12 +12,12 @@ console.log(k-b);
 console.log(k*b);
 console.log(k/b);
 console.log(k%b);
-console.log("a to power b ",k**b);
+console.log("k to power b ",k**b);
 
 let ab=5;
 let xy="5";
 console.log(ab==xy);//returns true
-console.log(ab===xy);
+console.log(ab===xy);//strict equality operator, returns false for different types of data
 console.log(ab!==xy);
 
 
